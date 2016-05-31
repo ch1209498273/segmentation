70c119a0638bdb26080b7a1c0b10b7c0316f63a3
xwzhong <artorius.mailbox@qq.com>
2016-05-31 10:07:09 +0800

    Create gen_pfdict.py

---
+++ gen_pfdict.py
@@ -0,0 +1,20 @@
+def gen_pfdict(self, f):
+        lfreq = {}
+        ltotal = 0
+        f_name = resolve_filename(f)
+        for lineno, line in enumerate(f, 1):
+            try:
+                line = line.strip().decode('utf-8')
+                word, freq = line.split(' ')[:2]
+                freq = int(freq)
+                lfreq[word] = freq
+                ltotal += freq
+                for ch in xrange(len(word)):
+                    wfrag = word[:ch + 1]
+                    if wfrag not in lfreq:
+                        lfreq[wfrag] = 0
+            except ValueError:
+                raise ValueError(
+                    'invalid dictionary entry in %s at Line %s: %s' % (f_name, lineno, line))
+        f.close()
+        return lfreq, ltotal
